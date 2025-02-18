924cbc45f2d6b490c5389b63fafd5f1990db8eb4
Alex <alex.holder@bigfootds.com>
2025-02-18 21:44:37 +1100

    ci: correct variables logged

--- src/utils/EnvironmentVariableHelper.js
+++ src/utils/EnvironmentVariableHelper.js
@@ -3,7 +3,7 @@ class Environment {
         this.BACKEND_CONTAINER_NAME = import.meta.env.VITE_BACKEND_CONTAINER_NAME;
 		this.BACKEND_CONTAINER_PORT = import.meta.env.VITE_BACKEND_CONTAINER_PORT;
 
-        console.log(this.VITE_BACKEND_CONTAINER_NAME, this.VITE_BACKEND_CONTAINER_PORT);
+        console.log(this.BACKEND_CONTAINER_NAME, this.BACKEND_CONTAINER_PORT);
     }
 }
 
