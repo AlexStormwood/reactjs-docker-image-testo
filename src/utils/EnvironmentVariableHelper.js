90faba021543044910d5083718c5c36797d01b74
Alex <alex.holder@bigfootds.com>
2025-02-18 21:49:14 +1100

    ci: remove reliance on build-time envs

--- src/utils/EnvironmentVariableHelper.js
+++ src/utils/EnvironmentVariableHelper.js
@@ -1,7 +1,7 @@
 class Environment {
     constructor() {
-        this.BACKEND_CONTAINER_NAME = import.meta.env.VITE_BACKEND_CONTAINER_NAME;
-		this.BACKEND_CONTAINER_PORT = import.meta.env.VITE_BACKEND_CONTAINER_PORT;
+        this.BACKEND_CONTAINER_NAME = "VITE_BACKEND_CONTAINER_NAME";
+		this.BACKEND_CONTAINER_PORT = "VITE_BACKEND_CONTAINER_PORT";
 
         console.log(this.BACKEND_CONTAINER_NAME, this.BACKEND_CONTAINER_PORT);
     }
